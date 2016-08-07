1e7956dd6ef7333f7c817d95144285ca23feffe3
masterUNG <phrombutr@gmail.com>
2016-08-07 11:04:30 +0700

    create class

--- app/src/main/java/appewtc/masterung/ungservice/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ungservice/MainActivity.java
@@ -1,6 +1,8 @@
 package appewtc.masterung.ungservice;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,6 +26,44 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
 
+    //Create Inner Class
+    private class SynchronizeUser extends AsyncTask<Void, Void, String> {
+
+        //Explicit
+        private Context context;
+        private String myUserString, myPasswordString;
+
+        public SynchronizeUser(Context context,
+                               String myUserString,
+                               String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return null;
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }   // onPost
+
+    }   // SynUser Class
+
+
     public void clickSignIn(View view) {
 
         userString = userEditText.getText().toString().trim();
@@ -35,9 +75,15 @@ public class MainActivity extends AppCompatActivity {
             myAlert.myDialog(this, 3, "Have Space", "Please Fill All Every Blank");
         } else {
 
+            //Call SynUser
+            SynchronizeUser synchronizeUser = new SynchronizeUser(this, userString, passwordString);
+            synchronizeUser.execute();
+
         }
 
     }   // clickSignIn
+
+
 
 
     public void clickSignUpMain(View view) {
